ae89f48a78156deb514186e1a6c8165b3e8d4cf5
crpreble <crpreble@gmail.com>
2021-11-29 12:10:39 -0500

    createsquare grid

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const pix = document.createElement('div');
+const gridBoard=document.querySelector('#grid-board')
+pix.classList.add('pix');
 
+function popGrid(size) {
+      area= size*size;
+      for (let i=0; i<area; i++){
+          gridBoard.appendChild(document.createElement('div'));
+      }
+}
+
+popGrid(16);
